15f77ec5b8a5cefc3cc49e2bd3f3fcc18ea2b1a6
gesofts <250058349@qq.com>
2018-01-17 23:10:08 +0800

    消息发送模块

--- site/src/main/java/com/xingyi/logistic/mq/SendMessageServer.java
+++ site/src/main/java/com/xingyi/logistic/mq/SendMessageServer.java
@@ -15,29 +15,81 @@ import java.io.ByteArrayOutputStream;
  * Created by WCL on 2018/1/12.
  */
 @Component
-public class SendMessageServer {
+public class SendMessageServer
+{
     @Autowired
     private JmsMessagingTemplate jmsMessagingTemplate;
 
+    //发送文本
+    private static String SEND_TERMINAL_MSG_QUEUE = "TEXTMESSAGESEND.QUEUE";
+
+    //下发调度
+    private static String DISPATCHPLANSEND_QUEUE = "DISPATCHPLANSEND.QUEUE";
+
+    //取消调度
+    private static String DISPATCHPLANSEND_CANCEL_QUEUE = "DISPATCHPLANSENDCANCEL.QUEUE";
 
     /**
-     * 向MQ发送消息
+     * 向终端下发调度任务
      * @param dispatchPlan
      * @throws JAXBException
      */
-    public void funSendMsg(DispatchPlan dispatchPlan) throws JAXBException
+    public void funSendMsg(DispatchPlan dispatchPlan)
     {
-        JAXBContext context = JAXBContext.newInstance(DispatchPlan.class);
-        Marshaller marshaller = context.createMarshaller(); // 根据上下文获取marshaller对象
 
-        //marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");  // 设置编码字符集
-        // marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true); // 格式化XML输出，有分行和缩进
-
-        marshaller.setProperty(Marshaller.JAXB_FRAGMENT, true);// 去掉生成xml的默认报文头
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        marshaller.marshal(dispatchPlan, baos);
-        String xmlObj = new String(baos.toByteArray());
-        System.out.println(xmlObj);
-        jmsMessagingTemplate.convertAndSend("DISPATCHPLANSEND.QUEUE", xmlObj);
+        String xmlObj = getXml(dispatchPlan.getClass(), dispatchPlan);
+        jmsMessagingTemplate.convertAndSend(DISPATCHPLANSEND_QUEUE, xmlObj);
     }
+
+    /**
+     * 向终端下发取消调度的命令
+     * @param dispatchPlan
+     */
+    public void funSendCancelMsg(DispatchPlan dispatchPlan)
+    {
+        String xmlObj = getXml(dispatchPlan.getClass(), dispatchPlan);
+        jmsMessagingTemplate.convertAndSend(DISPATCHPLANSEND_CANCEL_QUEUE, xmlObj);
+    }
+
+
+    /**
+     * 下发文本调度
+     * @param obj
+     */
+    public void funTerminalMsg(Object obj)
+    {
+        String xmlObj = getXml(obj.getClass(), obj);
+        jmsMessagingTemplate.convertAndSend(SEND_TERMINAL_MSG_QUEUE, xmlObj);
+    }
+
+
+
+    /**
+     * 处理消息中的XML内容
+     * @param cls
+     * @param mObj
+     * @return
+     */
+    public String getXml(Class cls, Object mObj)
+    {
+        String retXml = "";
+        try
+        {
+            JAXBContext context = JAXBContext.newInstance(cls);
+            Marshaller marshaller = context.createMarshaller(); // 根据上下文获取marshaller对象
+            //marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");  // 设置编码字符集
+            // marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true); // 格式化XML输出，有分行和缩进
+            marshaller.setProperty(Marshaller.JAXB_FRAGMENT, true);// 去掉生成xml的默认报文头
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            marshaller.marshal(mObj, baos);
+            retXml = new String(baos.toByteArray());
+            System.out.println(retXml);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return retXml;
+    }
+
 }
