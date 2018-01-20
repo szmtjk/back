87ec4a4a137cf382899d1f7091b4e66e5978f7c8
tsingtao <tsingtao_tung@163.com>
2018-01-21 03:10:55 +0800

    权限模块基础数据 CRUD

---
+++ business/src/main/java/com/xingyi/logistic/authentication/db/entity/UserProfileDO.java
@@ -0,0 +1,12 @@
+package com.xingyi.logistic.authentication.db.entity;
+
+
+import com.xingyi.logistic.business.bean.BaseModelAndDO;
+
+/**
+ * @author tsingtao_tung
+ * Created At: 2018/1/21 上午12:27.
+ */
+public class UserProfileDO extends BaseModelAndDO {
+
+}
