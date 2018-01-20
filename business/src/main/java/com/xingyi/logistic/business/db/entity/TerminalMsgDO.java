0babd23b7e23116885e01f4d82a08f91152c095c
1107323889 <1107323889@qq.com>
2018-01-20 15:02:56 +0800

    新增消息下发 修改图片上传大小限制和返回图片全路径

---
+++ business/src/main/java/com/xingyi/logistic/business/db/entity/TerminalMsgDO.java
@@ -0,0 +1,7 @@
+package com.xingyi.logistic.business.db.entity;
+
+import com.xingyi.logistic.business.bean.BaseTerminalMsg;
+import com.xingyi.logistic.business.model.TerminalMsg;
+
+public class TerminalMsgDO extends BaseTerminalMsg{
+}
