a1d03480e7debad6204575d64d6391782ead9afd
tsingtao <tsingtao_tung@163.com>
2018-01-23 00:57:29 +0800

    filter 支持跨域

--- site/src/main/java/com/xingyi/logistic/filter/AuthenticationFilter.java
+++ site/src/main/java/com/xingyi/logistic/filter/AuthenticationFilter.java
@@ -12,6 +12,7 @@ import org.springframework.core.annotation.Order;
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -32,6 +33,16 @@ public class AuthenticationFilter implements Filter {
 		response.setCharacterEncoding("UTF-8");
 		response.setContentType("application/json; charset=utf-8");
 		PrintWriter writer = response.getWriter();
+
+		//跨域设置
+		HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+		//这里填写你允许进行跨域的主机ip
+		httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
+		//允许的访问方法
+		httpServletResponse.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, PATCH");
+		//Access-Control-Max-Age 用于 CORS 相关配置的缓存
+		httpServletResponse.setHeader("Access-Control-Max-Age", "3600");
+		httpServletResponse.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
 		HttpServletRequest httpRequest = (HttpServletRequest) request;
 		String requestURI = httpRequest.getRequestURI();
