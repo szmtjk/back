7ac8a7f958dcacd019829a259b8dd2033f88b3b3
1107323889 <1107323889@qq.com>
2018-01-22 22:03:57 +0800

    系统登录后台代码修改

--- business/src/main/java/com/xingyi/logistic/authentication/model/LocalAuthQuery.java
+++ business/src/main/java/com/xingyi/logistic/authentication/model/LocalAuthQuery.java
@@ -3,21 +3,5 @@ package com.xingyi.logistic.authentication.model;
 import com.xingyi.logistic.business.bean.BaseQueryPage;
 
 public class LocalAuthQuery extends BaseQueryPage {
-	private String key;
 
-	public LocalAuthQuery(){
-
-	}
-
-	public LocalAuthQuery(String key){
-		this.key = key;
-	}
-
-	public String getKey() {
-		return key;
-	}
-
-	public void setKey(String key) {
-		this.key = key;
-	}
 }
