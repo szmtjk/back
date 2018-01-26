9813827ac04e46fbd30bfe6ca8651fada5a5a1fe
1107323889 <1107323889@qq.com>
2018-01-26 23:09:16 +0800

    系统登录后台代码修改

---
+++ business/src/main/java/com/xingyi/logistic/business/db/entity/LeftDispatchInfoDO.java
@@ -0,0 +1,9 @@
+package com.xingyi.logistic.business.db.entity;
+
+import com.xingyi.logistic.business.bean.BaseLeftDispatchInfo;
+
+/**
+ * 余量临调信息
+ */
+public class LeftDispatchInfoDO extends BaseLeftDispatchInfo{
+}
