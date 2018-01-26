f854e83eaacab62772d06b2e47e9c76afda4ad86
tsingtao <tsingtao_tung@163.com>
2018-01-26 23:58:06 +0800

    查询用户信息接口

--- business/src/main/java/com/xingyi/logistic/authentication/bean/BaseUserRoles.java
+++ business/src/main/java/com/xingyi/logistic/authentication/bean/BaseUserRoles.java
@@ -3,23 +3,23 @@ package com.xingyi.logistic.authentication.bean;
 import com.xingyi.logistic.business.bean.BaseModelAndDO;
 
 public class BaseUserRoles extends BaseModelAndDO{
-    private Integer userId;
-    private Integer roleId;
+    private Long userId;
+    private Long roleId;
     private Integer isDeleted;
 
-    public Integer getUserId() {
+    public Long getUserId() {
         return userId;
     }
 
-    public void setUserId(Integer userId) {
+    public void setUserId(Long userId) {
         this.userId = userId;
     }
 
-    public Integer getRoleId() {
+    public Long getRoleId() {
         return roleId;
     }
 
-    public void setRoleId(Integer roleId) {
+    public void setRoleId(Long roleId) {
         this.roleId = roleId;
     }
 
--- business/src/main/java/com/xingyi/logistic/authentication/service/UserRolesService.java
+++ business/src/main/java/com/xingyi/logistic/authentication/service/UserRolesService.java
@@ -4,5 +4,14 @@ import com.xingyi.logistic.authentication.model.UserRoles;
 import com.xingyi.logistic.authentication.model.UserRolesQuery;
 import com.xingyi.logistic.business.service.BaseService;
 
+import java.util.List;
+
 public interface UserRolesService extends BaseService<UserRoles,UserRolesQuery> {
+    /**
+     * 删除指定用户的角色配置
+     * @param userId
+     */
+    void deleteByUserId(Long userId);
+
+    int setRoles(Long userId, List<Long> roleIds);
 }
