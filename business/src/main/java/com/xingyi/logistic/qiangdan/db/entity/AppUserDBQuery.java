d6b5b9c2b9a3208960aaf8e5fb24d6e6e153c9e8
1107323889 <1107323889@qq.com>
2018-01-27 13:36:33 +0800

    系统登录后台代码修改

---
+++ business/src/main/java/com/xingyi/logistic/qiangdan/db/entity/AppUserDBQuery.java
@@ -0,0 +1,15 @@
+package com.xingyi.logistic.qiangdan.db.entity;
+
+import com.xingyi.logistic.business.bean.BaseDBQueryPage;
+
+public class AppUserDBQuery extends BaseDBQueryPage {
+    private String key;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
