6e76700d712cab8c7ea3bf38463326c08d57b8c1
tsingtao <tsingtao_tung@163.com>
2018-01-28 16:56:34 +0800

    增加临时打印输出，方便排查线上问题

--- business/src/main/java/com/xingyi/logistic/authentication/util/SessionUtil.java
+++ business/src/main/java/com/xingyi/logistic/authentication/util/SessionUtil.java
@@ -3,6 +3,8 @@ package com.xingyi.logistic.authentication.util;
 import com.xingyi.logistic.authentication.model.UserProfile;
 import com.xingyi.logistic.authentication.security.SecuritySession;
 import com.xingyi.logistic.authentication.security.Subject;
+import com.xingyi.logistic.authentication.security.User;
+import com.xingyi.logistic.qiangdan.model.AppUser;
 
 /**
  * @author tsingtao_tung
@@ -15,14 +17,32 @@ public final class SessionUtil {
 	}
 
 	public static SecuritySession getSession(){
-		return SecuritySession.getInstance();
+		SecuritySession session = SecuritySession.getInstance();
+		System.out.println(">>>>>>>>>>>>>>>>>" + SessionUtil.class.getName() + "获取SecuritySession:" + session);
+		return session;
 	}
 
 	public static Subject getSubject(){
-		return getSession().getSubject();
+		Subject subject = getSession().getSubject();
+		System.out.println(">>>>>>>>>>>>>>>>>" + SessionUtil.class.getName() + "获取subject:" + subject);
+		return subject;
+	}
+
+	public static User getUser(){
+		User user = getSubject().getUser();
+		System.out.println(">>>>>>>>>>>>>>>>>" + SessionUtil.class.getName() + "获取user:" + user);
+		return user;
 	}
 
 	public static UserProfile getProfile(){
-	    return getSubject().getUser().getProfile();
+		UserProfile profile = getUser().getProfile();
+		System.out.println(">>>>>>>>>>>>>>>>>" + SessionUtil.class.getName() + "获取profile:" + profile);
+	    return profile;
+    }
+
+    public static AppUser getAppUser(){
+		AppUser appUser = getSubject().getAppUser();
+	    System.out.println(">>>>>>>>>>>>>>>>>" + SessionUtil.class.getName() + "获取appUser:" + appUser);
+	    return appUser;
     }
 }
