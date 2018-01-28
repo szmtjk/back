39bd845804b3460a2776a052ae1619e5c05b2c42
xiaohu <xiaohu@meili-inc.com>
2018-01-28 11:15:42 +0800

    预约查询

--- business/src/main/java/com/xingyi/logistic/qiangdan/db/entity/ReservationDBQuery.java
+++ business/src/main/java/com/xingyi/logistic/qiangdan/db/entity/ReservationDBQuery.java
@@ -3,13 +3,31 @@ package com.xingyi.logistic.qiangdan.db.entity;
 import com.xingyi.logistic.business.bean.BaseDBQueryPage;
 
 public class ReservationDBQuery extends BaseDBQueryPage {
-    private String key;
+    private Integer leftDispatchId;
+    private Integer userId;
+    private Integer status;
 
-    public String getKey() {
-        return key;
+    public Integer getLeftDispatchId() {
+        return leftDispatchId;
     }
 
-    public void setKey(String key) {
-        this.key = key;
+    public void setLeftDispatchId(Integer leftDispatchId) {
+        this.leftDispatchId = leftDispatchId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
     }
 }
--- business/src/main/java/com/xingyi/logistic/qiangdan/model/ReservationQuery.java
+++ business/src/main/java/com/xingyi/logistic/qiangdan/model/ReservationQuery.java
@@ -4,4 +4,31 @@ import com.xingyi.logistic.business.bean.BaseQueryPage;
 
 public class ReservationQuery extends BaseQueryPage {
 
+    private Integer leftDispatchId;
+    private Integer userId;
+    private Integer status;
+
+    public Integer getLeftDispatchId() {
+        return leftDispatchId;
+    }
+
+    public void setLeftDispatchId(Integer leftDispatchId) {
+        this.leftDispatchId = leftDispatchId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
 }
