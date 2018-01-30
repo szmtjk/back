1d727b496a27cd5eb30ce57a0bba11a6b8092353
tsingtao <tsingtao_tung@163.com>
2018-01-30 11:56:02 +0800

    代码调整

--- business/src/main/java/com/xingyi/logistic/authentication/authenticator/impl/OAuthAuthenticator.java
+++ business/src/main/java/com/xingyi/logistic/authentication/authenticator/impl/OAuthAuthenticator.java
@@ -20,12 +20,12 @@ import org.springframework.util.Base64Utils;
  * @author tsingtao_tung
  * Created At: 2018/1/22 上午3:01.
  */
-public class OauthAuthenticator implements Authenticator {
+public class OAuthAuthenticator implements Authenticator {
 	private AppUserService appUserService;
 	private OAuthService oAuthService;
 	private WeiXinService weiXinService;
 
-	public OauthAuthenticator(){
+	public OAuthAuthenticator(){
 		this.appUserService = (AppUserService) ApplicationContextUtil.getBean("appUserServiceImpl");
 		this.oAuthService = (OAuthService) ApplicationContextUtil.getBean("oauthServiceImpl");
 		this.weiXinService = (WeiXinService) ApplicationContextUtil.getBean("weiXinServiceImpl");
