65779eacfdce719fd3f63c773afbc22c8b849075
Tao <bitao.ux@gmail.com>
2018-04-04 19:53:54 +0800

    Add pid file.

--- site/src/main/java/com/xingyi/logistic/Application.java
+++ site/src/main/java/com/xingyi/logistic/Application.java
@@ -1,7 +1,9 @@
 package com.xingyi.logistic;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.system.ApplicationPidFileWriter;
 import org.springframework.boot.web.support.ErrorPageFilter;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.web.context.WebApplicationContext;
@@ -9,15 +11,20 @@ import org.springframework.web.context.WebApplicationContext;
 /**
  * Created by Jadic on 2017/6/7.
  */
+@SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String... args) {
-        SpringApplication.run(MycWebConfig.class, args);
+        SpringApplication springApplication = new SpringApplication(Application.class);
+        springApplication.addListeners(new ApplicationPidFileWriter("pid.file"));
+        springApplication.run(args);
     }
 
     @Override
     protected WebApplicationContext run(SpringApplication application) {
         application.getSources().remove(ErrorPageFilter.class);
+        application.addListeners(new ApplicationPidFileWriter());
+
         return super.run(application);
     }
 
