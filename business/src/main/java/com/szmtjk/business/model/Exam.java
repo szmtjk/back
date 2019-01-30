22b5a7d5bf43bb9ba50f9a81b0782314f0044f2d
唐加利 <tangjiali5@163.com>
2019-01-31 00:58:17 +0800

    替换摘要中的换行符

--- business/src/main/java/com/szmtjk/business/model/Exam.java
+++ business/src/main/java/com/szmtjk/business/model/Exam.java
@@ -5,6 +5,12 @@ import com.szmtjk.business.util.DateUtils;
 
 public class Exam extends BaseExam {
 
+    @Override
+    public String getSummary() {
+        String summary = super.getSummary();
+        return summary == null ? summary : summary.replaceAll("\n", "<br/>");
+    }
+
     public String getSummaryDateStr(){
         return DateUtils.format(this.getSummaryDate(), "yyyy-MM-dd");
     }
