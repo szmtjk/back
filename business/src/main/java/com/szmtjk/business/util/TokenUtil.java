8887796f190673521e4d92f348ae8270af5ea2a1
xiaohu <xiaohu@meili-inc.com>
2019-03-06 00:41:29 +0800

    update

--- business/src/main/java/com/szmtjk/business/util/TokenUtil.java
+++ business/src/main/java/com/szmtjk/business/util/TokenUtil.java
@@ -15,10 +15,11 @@ public class TokenUtil {
     private static final long TOKEN_EXPIRE_TIME = 30 * 24 * 3600 * 1000L;// token有效期30天
 
     public static String encodeUserToken(User user) {
+
         long expire = System.currentTimeMillis() + TOKEN_EXPIRE_TIME;
         String md5 = DigestUtil.md5(String.valueOf(user.getId()), user.getUserName(), String.valueOf(expire));
         String token = user.getId() + ":" + md5 + ":" + expire + ":" + Authenticator.USER_TYPE_WE_CHAT;
-        token = Base64.getUrlEncoder().encodeToString(token.getBytes());
+        token = Base64.getUrlEncoder().withoutPadding().encodeToString(token.getBytes());
         return token;
     }
 
@@ -26,14 +27,14 @@ public class TokenUtil {
         long expire = System.currentTimeMillis() + TOKEN_EXPIRE_TIME;
         String md5 = DigestUtil.md5(String.valueOf(user.getId()), user.getLoginName(), user.getPasswd(), String.valueOf(expire));
         String token = user.getId() + ":" + md5 + ":" + expire + ":" + Authenticator.USER_TYPE_WEB_USER;
-        token = Base64.getUrlEncoder().encodeToString(token.getBytes());
+        token = Base64.getUrlEncoder().withoutPadding().encodeToString(token.getBytes());
         return token;
     }
 
     public static List<String> decodeUserToken(String token) {
         if (!StringUtils.isEmpty(token)) {
-            token = new String(Base64.getUrlDecoder().decode(token));
-            String[] tokenMembers = token.split(":");
+            String tokenDecoded = new String(Base64.getUrlDecoder().decode(token.getBytes()));
+            String[] tokenMembers = tokenDecoded.split(":");
             return Lists.newArrayList(tokenMembers);
         }
         return null;
