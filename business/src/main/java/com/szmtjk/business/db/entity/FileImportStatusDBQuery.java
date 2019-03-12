b8c7dad022ad5d0a2131c9c27f7cfa5844d65628
xiaohu <xiaohu@meili-inc.com>
2019-03-12 23:29:07 +0800

    update

--- business/src/main/java/com/szmtjk/business/db/entity/FileImportStatusDBQuery.java
+++ business/src/main/java/com/szmtjk/business/db/entity/FileImportStatusDBQuery.java
@@ -7,4 +7,28 @@ import com.szmtjk.business.bean.base.BaseDBQueryPage;
  */
 public class FileImportStatusDBQuery extends BaseDBQueryPage {
 
+    /**
+     * 导入业务类型  1：预约文件   2：体检报告
+     */
+    private Integer bizType;
+    /**
+     * 文件名
+     */
+    private String fileName;
+
+    public Integer getBizType() {
+        return bizType;
+    }
+
+    public void setBizType(Integer bizType) {
+        this.bizType = bizType;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
 }
