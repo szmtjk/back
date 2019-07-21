dd6150bb0218aa38b0192425e9e26f6dd893dbe5
jadic <jadic@163.com>
2019-07-21 16:26:32 +0800

    update

--- web/src/main/java/com/szmtjk/web/util/TestUtil2.java
+++ web/src/main/java/com/szmtjk/web/util/TestUtil2.java
@@ -1,7 +1,7 @@
 package main.java.com.szmtjk.web.util;
 
 public class TestUtil2 {
-	public void test1() {
-		System.out.println("blabla");
-	}
+    public void test1() {
+        System.out.println("blabla");
+    }
 }
--- web/src/main/java/com/szmtjk/web/util/TestUtil3.java
+++ web/src/main/java/com/szmtjk/web/util/TestUtil3.java
@@ -1,7 +1,7 @@
 package main.java.com.szmtjk.web.util;
 
 public class TestUtil3 {
-	public void test1() {
-		System.out.println("test1 blab");
-	}
+    public void test1() {
+        System.out.println("test1 blab");
+    }
 }
