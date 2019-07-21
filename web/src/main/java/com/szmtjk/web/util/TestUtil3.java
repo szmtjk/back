3ba561f84442f4e0ddb1c34f06505a45cb959a57
jadic <jadic@163.com>
2019-07-21 16:28:14 +0800

    update

--- web/src/main/java/com/szmtjk/web/util/TestUtil3.java
+++
@@ -1,7 +0,0 @@
-package main.java.com.szmtjk.web.util;
-
-public class TestUtil3 {
-    public void test1() {
-        System.out.println("test1 blab");
-    }
-}
--- web/src/main/java/com/szmtjk/web/util/TestUtil4.java
+++
@@ -1,8 +0,0 @@
-package main.java.com.szmtjk.web.util;
-
-public class TestUtil4 {
-
-    public static void main(String[] args) {
-        System.out.println("hi");
-    }
-}
